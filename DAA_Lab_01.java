f51c09b4b08e2b55f65f4ecc5adbd19189b3deb0
Vedansh Agarwal <101228748+vedansh-agarwal@users.noreply.github.com>
2022-03-09 06:48:27 +0530

    Add files via upload
    Implementation of Kruskal Algorithm using Java

---
+++ DAA_Lab_01.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class DAA_Lab_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no. of nodes: ");
+        int nodes = sc.nextInt();
+        int temp, min, i, j, cost = 0;
+
+        System.out.println("Enter Matrix Input for graph:");
+        for(i = 0; i < nodes; i++)
+        {
+            for(j = 0, min = Integer.MAX_VALUE; j < nodes; j++)
+            {
+                temp = sc.nextInt();
+                if(temp != -1 && j > i && temp < min)
+                    min = temp;
+            }
+            if(i < nodes - 1)
+                cost += min;
+        }
+        System.out.print("Minimum Spanning Tree Cost: "+cost);
+    }
+}
