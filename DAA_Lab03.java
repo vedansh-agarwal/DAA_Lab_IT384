c01c158ec9b6e08f5b0de71e63c665e8be5a1367
Vedansh Agarwal <101228748+vedansh-agarwal@users.noreply.github.com>
2022-03-31 21:53:29 +0530

    Update and rename DAA_Lab_03.java to DAA_Lab03.java

--- DAA_Lab03.java
+++ DAA_Lab03.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class DAA_Lab_03 {
+public class DAA_Lab03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter no. of rows in matrix 1: ");
